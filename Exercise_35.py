c5d8b7fe3f34d1c9eb77f768249778eb7329fa17
LondonFC <tottham@163.com>
2018-06-26 21:47:43 -0400

    beat 91.31% submmisions

---
+++ Exercise_35.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 23 22:37:02 2018
+
+@author: ruicao
+"""
+
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums or target is None:
+            return 0
+        
+        i=0
+        j=len(nums)-1
+        
+        while i<j:
+            mid = (j-i)//2+i
+            
+            if nums[mid]>target:
+                j=mid
+            elif nums[mid]<target:
+                i=mid+1
+        
+            else:
+                return mid
+            
+        if i>=j:
+            if nums[i]<target:
+                return i+1
+            return i
